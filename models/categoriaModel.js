546ab4ef9f07c9a82ac479e6d3c85a5e00928e8b
Candela Quiroga <candeequiroga02@gmail.com>
2024-08-31 14:52:25 -0300

    Corrección código duplicado

--- models/categoriaModel.js
+++ models/categoriaModel.js
@@ -13,18 +13,3 @@ class CategoriaModel {
 };
 
 module.exports = CategoriaModel;
-const conx= require('../database/db');
-
-class CategoriaModel {
-
-    async listar(callback){
-        let sql = "SELECT * FROM categoria_permiso";
-
-        conx.query(sql, [], async(err,results) => {
-            callback(results);
-        });
-
-    }
-};
-
-module.exports = CategoriaModel;

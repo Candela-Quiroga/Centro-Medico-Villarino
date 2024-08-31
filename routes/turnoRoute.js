4c58a52f94e73b59ff6f12a01411586ce2c1d9a2
Junior Acuña <junioralexanderacunaespinola@gmail.com>
2024-08-31 12:10:32 -0300

    Agrego los controladores, rutas y modelos de Turnos y Medicos.
    Estos son los que no pude subir el otro día. Todavía falta acomodarlos porque aun no funcionan.

---
+++ routes/turnoRoute.js
@@ -0,0 +1,11 @@
+//AGREGAREMOS LOS COMENTARIOS DE TODO EN CADA LINEA UNA VEZ NOS FUNCIONE EL ABML
+
+const express = require ('express');
+const router = express.Router();
+
+
+//A desarrollar
+
+
+
+module.exports = turnoRoute //es para exportar el modulo.

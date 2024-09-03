498ad8fc7c3f9dff611027d95d045fbe04ed95f9
Candela Quiroga <candeequiroga02@gmail.com>
2024-09-03 15:41:17 -0300

    modificaciones
    se pusieron las foreign key, las primary key y los auto_increment dentro de las tablas

--- centromedico (3).sql
+++ centromedico (3).sql
@@ -11,7 +11,6 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -21,20 +20,15 @@ SET time_zone = "+00:00";
 -- Database: `centromedico`
 --
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `categoria_permiso`
---
 
 CREATE TABLE `categoria_permiso` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
 -- Dumping data for table `categoria_permiso`
---
 
 INSERT INTO `categoria_permiso` (`id`, `nombre`) VALUES
 (1, 'Administrador'),
@@ -42,35 +36,28 @@ INSERT INTO `categoria_permiso` (`id`, `nombre`) VALUES
 (3, 'Secretaria'),
 (4, 'Paciente');
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `disponibilidad_medicos`
---
 
 CREATE TABLE `disponibilidad_medicos` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_medico` int(11) NOT NULL,
   `dia_semana` varchar(255) NOT NULL,
   `hora_inicio` time NOT NULL,
   `hora_fin` time NOT NULL,
-  `minutos_turno` time NOT NULL
+  `minutos_turno` time NOT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`id_medico`) REFERENCES `medicos`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `especialidades`
---
 
 CREATE TABLE `especialidades` (
-  `id` int(11) NOT NULL,
-  `nombre_especialidad` varchar(255) NOT NULL
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre_especialidad` varchar(255) NOT NULL,
+  PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
 -- Dumping data for table `especialidades`
---
 
 INSERT INTO `especialidades` (`id`, `nombre_especialidad`) VALUES
 (1, 'oftalmología'),
@@ -81,45 +68,38 @@ INSERT INTO `especialidades` (`id`, `nombre_especialidad`) VALUES
 (6, 'Clínica médica'),
 (7, 'Nutrición');
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `medicos`
---
 
 CREATE TABLE `medicos` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_usuario` int(11) NOT NULL,
   `id_especialidad` int(11) NOT NULL,
-  `telefono` varchar(255) NOT NULL
+  `telefono` varchar(255) NOT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`id_usuario`) REFERENCES `usuarios`(`id`),
+  FOREIGN KEY (`id_especialidad`) REFERENCES `especialidades`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `medicos_obrassociales`
---
 
 CREATE TABLE `medicos_obrassociales` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_medico` int(11) NOT NULL,
-  `id_obraSocial` int(11) NOT NULL
+  `id_obraSocial` int(11) NOT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`d_medico`) REFERENCES `medicos`(`id`),
+  FOREIGN KEY (`id_obraSocial`) REFERENCES `obras_Sociales`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `obras_sociales`
---
 
 CREATE TABLE `obras_sociales` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
 -- Dumping data for table `obras_sociales`
---
 
 INSERT INTO `obras_sociales` (`id`, `nombre`) VALUES
 (4, 'Avalian'),
@@ -133,52 +113,43 @@ INSERT INTO `obras_sociales` (`id`, `nombre`) VALUES
 (9, 'Sancor Salud'),
 (3, 'SWISS Medical');
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `pacientes`
---
 
 CREATE TABLE `pacientes` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `telefono` varchar(255) NOT NULL,
   `id_obrasocial` int(11) NOT NULL,
-  `nro_afiliado` int(255) NOT NULL
+  `nro_afiliado` int(255) NOT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`id_obraSocial`) REFERENCES `obras_Sociales`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `turnos`
---
 
 CREATE TABLE `turnos` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_medico` int(11) NOT NULL,
   `id_paciente` int(11) NOT NULL,
   `fecha_hora` datetime NOT NULL,
-  `motivo` varchar(255) NOT NULL
+  `motivo` varchar(255) NOT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`id_medico`) REFERENCES `medicos`(`id`),
+  FOREIGN KEY (`id_paciente`) REFERENCES `pacientes`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
 -- Table structure for table `usuarios`
---
 
 CREATE TABLE `usuarios` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `id_categoriaPermiso` int(11) NOT NULL
+  `id_categoriaPermiso` int(11) NOT NULL,
+  PRIMARY KEY(`id`),
+  FOREIGN KEY (`id_categoriaPermiso`) REFERENCES `categoria_permiso`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `usuarios`
---
 
 INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`, `id_categoriaPermiso`) VALUES
 (4, 'Esteban Mazzaro', 'mazzaroesteban@gmail.com', '', 2),
@@ -191,176 +162,6 @@ INSERT INTO `usuarios` (`id`, `nombre`, `email`, `password`, `id_categoriaPermis
 (11, 'Junior Acuña', 'acuñajunior@gmail.com', '', 1),
 (12, 'Camila Mazzaro', 'mazzarocamila@gmail.com', '', 3),
 (13, 'Agustin Carrasco', 'carrascoagustin@gmail.com', '', 3);
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `categoria_permiso`
---
-ALTER TABLE `categoria_permiso`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `disponibilidad_medicos`
---
-ALTER TABLE `disponibilidad_medicos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_medico` (`id_medico`);
-
---
--- Indexes for table `especialidades`
---
-ALTER TABLE `especialidades`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `medicos`
---
-ALTER TABLE `medicos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_usuario` (`id_usuario`),
-  ADD KEY `id_especialidad` (`id_especialidad`);
-
---
--- Indexes for table `medicos_obrassociales`
---
-ALTER TABLE `medicos_obrassociales`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_medico` (`id_medico`),
-  ADD KEY `id_obraSocial` (`id_obraSocial`);
-
---
--- Indexes for table `obras_sociales`
---
-ALTER TABLE `obras_sociales`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nombre` (`nombre`);
-
---
--- Indexes for table `pacientes`
---
-ALTER TABLE `pacientes`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_obrasocial` (`id_obrasocial`);
-
---
--- Indexes for table `turnos`
---
-ALTER TABLE `turnos`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_medico` (`id_medico`),
-  ADD KEY `id_paciente` (`id_paciente`);
-
---
--- Indexes for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_categoriaPermiso` (`id_categoriaPermiso`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `categoria_permiso`
---
-ALTER TABLE `categoria_permiso`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `disponibilidad_medicos`
---
-ALTER TABLE `disponibilidad_medicos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `especialidades`
---
-ALTER TABLE `especialidades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- AUTO_INCREMENT for table `medicos`
---
-ALTER TABLE `medicos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `medicos_obrassociales`
---
-ALTER TABLE `medicos_obrassociales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `obras_sociales`
---
-ALTER TABLE `obras_sociales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
-
---
--- AUTO_INCREMENT for table `pacientes`
---
-ALTER TABLE `pacientes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `turnos`
---
-ALTER TABLE `turnos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `usuarios`
---
-ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `disponibilidad_medicos`
---
-ALTER TABLE `disponibilidad_medicos`
-  ADD CONSTRAINT `disponibilidad_medicos_ibfk_1` FOREIGN KEY (`id_medico`) REFERENCES `medicos` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `medicos`
---
-ALTER TABLE `medicos`
-  ADD CONSTRAINT `medicos_ibfk_1` FOREIGN KEY (`id_especialidad`) REFERENCES `especialidades` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `medicos_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `medicos_obrassociales`
---
-ALTER TABLE `medicos_obrassociales`
-  ADD CONSTRAINT `medicos_obrassociales_ibfk_1` FOREIGN KEY (`id_medico`) REFERENCES `medicos` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `medicos_obrassociales_ibfk_2` FOREIGN KEY (`id_obraSocial`) REFERENCES `obras_sociales` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `pacientes`
---
-ALTER TABLE `pacientes`
-  ADD CONSTRAINT `pacientes_ibfk_1` FOREIGN KEY (`id_obrasocial`) REFERENCES `obras_sociales` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `turnos`
---
-ALTER TABLE `turnos`
-  ADD CONSTRAINT `turnos_ibfk_1` FOREIGN KEY (`id_paciente`) REFERENCES `pacientes` (`id`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `turnos_ibfk_2` FOREIGN KEY (`id_medico`) REFERENCES `medicos` (`id`) ON UPDATE CASCADE;
-
---
--- Constraints for table `usuarios`
---
-ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_categoriaPermiso`) REFERENCES `categoria_permiso` (`id`) ON UPDATE CASCADE;
-COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

17052d05300f482f40191c53a821fedfb3b23348
Camila Mazzaro <camilamazzaro90@gmail.com>
2024-10-03 01:40:22 -0300

    Actualizaciones Front en Home.ejs

--- controllers/webController.js
+++ controllers/webController.js
@@ -1,8 +1,22 @@
+const MedicoModel = require('../models/medicoModel');
+const medicoModel = new MedicoModel();
+
+const PacienteModel = require('../models/PacienteModel');
+const pacienteModel = new PacienteModel();
+
+
 class WebController{
 
     //Mostrar homepage
-    mostrarHome(req,res){
-        res.render('web/home', {title: 'Centro Médico - Inicio' });
+    async mostrarHome(req,res){
+        medicoModel.listarMedicos((medicos) => {
+            res.render('web/home', {
+                title: 'Centro Médico Villarino - Home',
+                medicos: medicos // Pasamos los médicos a la vista
+            });
+            
+        })
+        
     }
 
     //Mostrar pagina nosotros
@@ -12,13 +26,38 @@ class WebController{
 
     //Mostrar pagina profesionales
     mostrarProfesionales(req,res){
-        res.render('web/profesionales', {title: 'Nuestros profesionales'});
+        medicoModel.listarMedicos((medicos) =>{
+            res.render('web/profesionales', {
+                title: 'Profesionales',
+                medicos: medicos
+            });
+        })
+        
     }
 
     //Mostrar pagina coberturas
-    mostrarCoberturas(req,res){
-        res.render('web/coberturas', {title: 'Coberturas Médicas'});
+
+    async mostrarCoberturas(req, res) {
+        try {
+            // Llamar a listarMedicos y obtenerObrasSociales en paralelo
+            medicoModel.listarMedicos((medicos) => {
+                pacienteModel.obtenerObrasSociales((obrasSociales) => {
+                    // Renderizar la vista de coberturas, pasando los médicos y las obras sociales
+                    res.render('web/coberturas', {
+                        title: 'Coberturas Médicas',
+                        medicos: medicos,
+                        obrasSociales: obrasSociales
+                    });
+                });
+            });
+
+        } catch (error) {
+            console.error('Error al obtener datos para la página de coberturas:', error);
+            res.status(500).send('Error al cargar la página de coberturas');
+        }
     }
+
+
 
     //Mostrar pagina contacto
     mostrarContacto(req,res){

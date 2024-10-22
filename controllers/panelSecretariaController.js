54022b2b09281631a77eb64bb37f2840df60eea1
Camila Mazzaro <camilamazzaro90@gmail.com>
2024-10-22 19:35:03 -0300

    Avances en el Panel General de Secretarias
    Hice un listado de médicos resumido y un buscador/select de obras sociales para ir filtrando los médicos de ese listado.

--- controllers/panelSecretariaController.js
+++ controllers/panelSecretariaController.js
@@ -1,11 +1,32 @@
 //panelController.js
-//acá va el control del administrador
+const MedicoModel = require('../models/medicoModel');
+const medicoModel = new MedicoModel();
+
+const PacienteModel = require('../models/PacienteModel');
+const pacienteModel = new PacienteModel();
 
 class PanelSecretariaController{
 
     //mostrar panel general
     mostrarPanelGeneral(req,res){
-        res.render('panel/panelSecretarias', {title: 'Panel General'});
+        try {
+            // Llamar a listarMedicos y obtenerObrasSociales en paralelo
+            medicoModel.listarMedicos((medicos) => {
+                pacienteModel.obtenerObrasSociales((obrasSociales) => {
+                    // Renderizar la vista de coberturas, pasando los médicos y las obras sociales
+                    res.render('panel/panelSecretarias', {
+                        title: 'Panel General Secretarias',
+                        medicos: medicos,
+                        obrasSociales: obrasSociales
+                    });
+                });
+            });
+
+        } catch (error) {
+            console.error('Error al obtener datos para la página de coberturas:', error);
+            res.status(500).send('Error al cargar la página de coberturas');
+        }
+        
     }
 }
 
